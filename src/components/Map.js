23b2a11d38ef2f6b32a6e4fb0379d33eb9fc714b
Ry <thomashenrypaert@gmail.com>
2026-03-26 14:41:07 +0200

    get user location, fixed some issues
    app now gets location based on users lat and lon
    fixed issue where camera would stay in africa
    commented out redundant camera position
    
    added a isMapReady check for the future

---
+++ src/components/Map.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useRef, useState, useEffect } from 'react';
+import { useLocation } from '../hooks/useLocation';
+
+const Map = () => {
+    const mapRef = useRef(null);
+    const {location, errorMsg} = useLocation();
+    const [isMapReady, setIsMapReady] = useState(false);
+
+    useEffect(() => {
+        if (isMapReady && location) {
+            mapRef.current?.animateCamera({
+                center: {
+                    latitude: location.latitude, 
+                    longitude: location.longitude
+                },
+                pitch: 0,
+                heading: 0,
+                altitude: 1,
+                zoom: 16
+            });
+        }
+    }, [isMapReady, location])
+        
+
+
+    return (
+        <MapView
+            ref={mapRef}
+            onMapReady={() => setIsMapReady(true)}
+            showsUserLocation={true}
+            followsUserLocation={true}
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            mapType='satellite'
+            // initialCamera={{
+            //     center: {
+            //         latitude: 59.22693030653677,
+            //         longitude: 24.138830513826036
+            //     },
+            // pitch: 0,
+            // heading: 0,
+            // altitude: 1,
+            // zoom: 16
+            // }}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFill,
+  }
+});
+
+export default Map;
