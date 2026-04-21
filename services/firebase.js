bad317ede83f82c114b47ae52650433c35905ca5
Ry <thomashenrypaert@gmail.com>
2026-04-21 13:33:39 +0300

    Added login persistence

--- services/firebase.js
+++ services/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   storageBucket: "",
@@ -10,6 +12,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence:getReactNativePersistence(AsyncStorage)
+});
+
+export const db = getFirestore(app);
